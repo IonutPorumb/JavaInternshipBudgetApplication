d6ac8542f4b03d9a87cf0ffdc1485485ac6b43ee
ionut.c.porumb <ionut_porumb@yahoo.com>
2022-05-21 08:35:13 +0300

    The TransactionApplication was modified for exception handeling and transaction validation and for the scheduler. The TransasctionService class in service flolder was modified acordingly.The server port was modified in application.properties

--- java/com/accenture/transactionapplication/TransactionApplication.java
+++ java/com/accenture/transactionapplication/TransactionApplication.java
@@ -1,14 +1,15 @@
-package com.accentureapplication;
+package com.accenture.transactionapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TransactionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TransactionApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionApplication.class, args);
+    }
 
 }
